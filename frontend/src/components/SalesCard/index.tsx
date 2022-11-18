f91bab93a1b5fcdbfe8c6309fc132099f5d8a85a
Lucas <lucas.lsc@hotmail.com.br>
2022-11-18 17:38:54 -0300

    send notification

--- frontend/src/components/SalesCard/index.tsx
+++ frontend/src/components/SalesCard/index.tsx
@@ -20,8 +20,7 @@ function SalesCard() {
     
     const dmin = minDate.toISOString().slice(0, 10);
     const dmax = maxDate.toISOString().slice(0, 10);
-
-    console.log(dmin)
+    
 
     axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`).then((response) => {
       setSales(response.data.content);
@@ -64,7 +63,7 @@ function SalesCard() {
             </tr>
           </thead>
           <tbody>
-            {sales.map((sale) => {
+            {sales.map(sale => {
               return (
                 <tr key={sale.id}>
                   <td className="show992">{sale.id}</td>
@@ -75,7 +74,7 @@ function SalesCard() {
                   <td>{sale.amount.toFixed(2)}</td>
                   <td>
                     <div className="dsmeta-red-btn-container">
-                      <NotificationButton />
+                      <NotificationButton saleId={sale.id} />
                     </div>
                   </td>
                 </tr>
